fac57cd34f2f355271de2c696d4686e8ae0b2584
Shashank <shashanke7y@gmail.com>
2023-09-03 12:03:51 -0400

    improved tokenization and bag of words functions

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,6 +1,6 @@
 import nltk
-nltk.download('punkt')
-
+# nltk.download('punkt')
+import numpy as np
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -10,5 +10,16 @@ def tokenize(sentence):
 def stem(word): 
     return stemmer.stem(word.lower())
 
-def bag_of_word(all_words):
-    pass
+def bag_of_words(tokenized_sentence, all_words):
+    """
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog = [    0 ,    1,     0,    1,     0,     0,       0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    
+    bag = np.zeros(len(all_words), dtype = np.float32)
+    for i, w in enumerate(all_words): 
+        if w in tokenized_sentence: 
+            bag[i] = 1.0
+    return bag
