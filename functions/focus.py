04b859a1d6728d70c07a2241f3447d023fa12c71
Shashank <shashanke7y@gmail.com>
2023-09-09 22:40:57 -0400

    closes chrome browser when focus is started and ended

--- functions/focus.py
+++ functions/focus.py
@@ -1,8 +1,11 @@
+import os
+import webbrowser
 host_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 
 websites = ['www.youtube.com', "https://www.youtube.com"]
-def start_focus(): 
+def start_focus():
+    os.system("taskkill /im chrome.exe /f")
     with open(host_path, "r+") as fileptr: 
         content = fileptr.read()
         for website in websites: 
@@ -11,9 +14,10 @@ def start_focus():
             else: 
                 fileptr.write(redirect+"      "+website+"\n")
     print("blocked all entertainment websites")            
-                
+    webbrowser.open("https://www.notion.so/Assignments-Exams-0c3135345c6d4b6aabfc8a7150b55767")
+
 def end_focus(): 
-    print("end focus")
+    os.system("taskkill /im chrome.exe /f")
     with open(host_path, 'r+') as fileptr:
             content=fileptr.readlines()
             fileptr.seek(0)
@@ -25,3 +29,4 @@ def end_focus():
             fileptr.truncate()
     
     print("unblocked all entertainment websites")
+    webbrowser.open("https://www.notion.so/Assignments-Exams-0c3135345c6d4b6aabfc8a7150b55767")
