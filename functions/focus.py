2e42021034cd81347f42cb232c9233eefcfb47ce
Shashank <shashanke7y@gmail.com>
2023-09-09 21:56:33 -0400

    blocks a youtube if a focus session is started and unblocks youtube after focus session has ended

--- functions/focus.py
+++ functions/focus.py
@@ -1,5 +1,27 @@
-def start_focus():
-    print("Start focus")
-    
+host_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+
+websites = ['www.youtube.com', "https://www.youtube.com"]
+def start_focus(): 
+    with open(host_path, "r+") as fileptr: 
+        content = fileptr.read()
+        for website in websites: 
+            if website in content: 
+                pass
+            else: 
+                fileptr.write(redirect+"      "+website+"\n")
+    print("blocked all entertainment websites")            
+                
 def end_focus(): 
-    print("End Focus")
+    print("end focus")
+    with open(host_path, 'r+') as fileptr:
+            content=fileptr.readlines()
+            fileptr.seek(0)
+            for line in content:
+                if not any(website in line for website in websites):
+                    fileptr.write(line)
+  
+            # removing hostnmes from host file
+            fileptr.truncate()
+    
+    print("unblocked all entertainment websites")
