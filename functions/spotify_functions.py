ae8f8c4502a2ab15b4b71917d8ea7ddc3acf05c3
Shashank <shashanke7y@gmail.com>
2023-09-15 23:05:38 -0400

    sends the tracking name, also artist name and link to the track

--- functions/spotify_functions.py
+++ functions/spotify_functions.py
@@ -14,6 +14,11 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                redirect_uri=REDIRECT_URI,
                                                scope='user-library-read'))
 
-def play_song(song): 
+async def play_song(song, user): 
     results = sp.search(q=song, type="track")
-    print(results)
+    if len(results['tracks']['items']) > 0:
+        first_track = results['tracks']['items'][0]
+        await user.send(f"Playing {first_track['name']}..., by ".join(artist['name'] for artist in first_track['artists']))
+        await user.send(first_track['external_urls']['spotify'])
+    else:
+        await user.send("No matching tracks found.")
