555b1dac13ae639e4acc722b9efc66f7a986198b
Shashank <shashanke7y@gmail.com>
2023-09-16 16:32:00 -0400

    jarvis can now play songs that you ask for! jarvisPlayback v1

--- functions/spotify_functions.py
+++ functions/spotify_functions.py
@@ -12,13 +12,17 @@ REDIRECT_URI = os.getenv('REDIRECT_URI')
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
                                                redirect_uri=REDIRECT_URI,
-                                               scope='user-library-read'))
+                                               scope='user-library-read, user-read-playback-state, user-modify-playback-state, user-read-currently-playing, app-remote-control, streaming'))
 
 async def play_song(song, user): 
     results = sp.search(q=song, type="track")
     if len(results['tracks']['items']) > 0:
         first_track = results['tracks']['items'][0]
+        track_uri = [first_track['uri']]
         await user.send(f"Playing {first_track['name']}")
         await user.send(first_track['external_urls']['spotify'])
+        devices = sp.devices()
+        device_id = devices['devices'][0]['id']
+        sp.start_playback(uris=track_uri, position_ms=0, device_id=device_id)
     else:
         await user.send("No matching tracks found.")
