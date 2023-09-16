81442558ccbeaf4ad33813a968935ff430b91d35
Shashank <shashanke7y@gmail.com>
2023-09-16 15:27:00 -0400

    remove artist name which displays artist name and not song title

--- functions/spotify_functions.py
+++ functions/spotify_functions.py
@@ -18,7 +18,7 @@ async def play_song(song, user):
     results = sp.search(q=song, type="track")
     if len(results['tracks']['items']) > 0:
         first_track = results['tracks']['items'][0]
-        await user.send(f"Playing {first_track['name']}..., by ".join(artist['name'] for artist in first_track['artists']))
+        await user.send(f"Playing {first_track['name']}")
         await user.send(first_track['external_urls']['spotify'])
     else:
         await user.send("No matching tracks found.")
