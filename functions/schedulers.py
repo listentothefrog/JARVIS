a08145eecca1b5c617f42d13bbd868ceea4745fa
Shashank <shashanke7y@gmail.com>
2023-10-01 21:08:13 -0400

    added a set of todos

---
+++ functions/schedulers.py
@@ -0,0 +1,11 @@
+# Todos
+# 5:46 AM Wake up computer 
+# 5:48 AM Start focus session 
+# 6:30 AM End focus session, turn off computer, shower process 
+# 4:00 PM Start computer, start focus session, homework process
+# 5:00 PM Start sending notifcations on any remaining events in calendar
+# 5:30 PM Start break functions till 5:40 PM 
+# 5:40 PM Start focus session till 7:00 PM 
+# 7:00 PM - 8:30 PM Start gym functions 
+# 9:00 PM - 9:45 PM Start focus session function
+# 9:46 PM Start bed time wrapup, turns off internet.
