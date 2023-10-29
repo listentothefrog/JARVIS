2d25cb8ccb674fc6ef9ad94801a44f767ec1c8e3
Shashank <shashanke7y@gmail.com>
2023-10-28 20:51:49 -0400

    announces the article title and prints the link to the console

--- functions/get_news.py
+++ functions/get_news.py
@@ -1,9 +1,16 @@
 import os
+import time
 from dotenv import find_dotenv, load_dotenv
 from newsapi import NewsApiClient
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 NEWS_API_KEY = os.getenv('NEWS_API')
+import pyttsx3
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices') #getProperty(‘voices’) returns an array of all available voices
+engine.setProperty('voice', voices[0].id)
+
 
 newsapi = NewsApiClient(api_key=NEWS_API_KEY)
 
@@ -11,12 +18,18 @@ def get_top_news_articles(source, num_articles=5):
     top_headlines = newsapi.get_top_headlines(sources=source)
     articles = top_headlines['articles'][:num_articles]
 
+    engine = pyttsx3.init()
+
     for i, article in enumerate(articles):
-        print(f"Article {i+1}:")
+        print(f"Article {i + 1}:")
         print(f"Title: {article['title']}")
         print(f"Description: {article['description']}")
-        print("")
+        print(article['url'])
+        engine.say(f"{article['title']}")
+        time.sleep(10)
+        print(article['url'])
+    
+    engine.say("I have just send the links for all these articles.")
+    engine.runAndWait()
 
-get_top_news_articles('cnn', num_articles=5)
-
-
+get_top_news_articles('bbc-news', num_articles=5)
