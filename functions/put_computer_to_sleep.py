01916a087035613a9ceb8ee3dae609922d27ddc3
Shashank <shashanke7y@gmail.com>
2023-11-04 21:39:43 -0400

    change file names

--- functions/put_computer_to_sleep.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def put_computer_to_sleep():
-    if os.name == 'posix':
-        os.system('sudo pmset sleepnow')
-    elif os.name == 'nt':
-        # For Windows
-        os.system('rundll32.exe powrprof.dll,SetSuspendState 0,1,0')
